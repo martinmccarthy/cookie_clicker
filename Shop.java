41bd4bf7115e31a8bfc30b6e0d86df8a59508f26
Martin McCarthy <martinmccarthy@knights.ucf.edu>
2021-01-01 15:25:34 -0500

    implements working shop functions to maintain proper app logic

--- Shop.java
+++ Shop.java
@@ -1,5 +1,16 @@
-import javax.swing.*;
-
 public class Shop {
+    public int subtractCookies(int currentCookies, int cost) {
+        return currentCookies - cost;
+    }
 
+    boolean validityCheck(int currentCookies, int cost) {
+        if(cost > currentCookies)
+            return false;
+        else
+            return true;
+    }
+
+    int newPrice(int multiplierPrice) {
+        return multiplierPrice * 4;
+    }
 }
